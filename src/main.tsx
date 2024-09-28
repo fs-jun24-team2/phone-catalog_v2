4fb5fdc17512e1b9e867a4bf019d75e29ffed2f4
Iryna Yehorova <yehorova.dev@gmail.com>
2024-09-28 14:45:44 +0300

    feat: add theme context

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import { I18nextProvider } from 'react-i18next';
 import { store } from './app/store';
 import i18n from './i18n';
 import { Root } from './Root';
+import { ThemeProvider } from './context/ThemeContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <I18nextProvider i18n={i18n}>
-        <Root />
-      </I18nextProvider>
+      <ThemeProvider>
+        <I18nextProvider i18n={i18n}>
+          <Root />
+        </I18nextProvider>
+      </ThemeProvider>
     </Provider>
   </StrictMode>,
 );
