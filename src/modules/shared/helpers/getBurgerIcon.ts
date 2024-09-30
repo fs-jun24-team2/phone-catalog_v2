84bb4c54617fb8dd7e5611fc3811185bc487ee99
Iryna Yehorova <yehorova.dev@gmail.com>
2024-09-30 13:15:47 +0300

    fix: correct icon url (#8)

--- src/modules/shared/helpers/getBurgerIcon.ts
+++ src/modules/shared/helpers/getBurgerIcon.ts
@@ -1,13 +1,14 @@
 import { Theme } from '@/types/Theme';
 
+
 const burgerIcons = {
   original: {
-    open: 'images/original/icons/original_burger_open.svg',
-    close: 'images/original/icons/original_burger_close.svg',
+    open: './images/original/icons/original_burger_open.svg',
+    close: './images/original/icons/original_burger_close.svg',
   },
   dark: {
-    open: 'images/dark/icons/dark_burger_open.svg',
-    close: 'images/dark/icons/dark_burger_close.svg',
+    open: './images/dark/icons/dark_burger_open.svg',
+    close: './images/dark/icons/dark_burger_close.svg',
   },
 };
 
