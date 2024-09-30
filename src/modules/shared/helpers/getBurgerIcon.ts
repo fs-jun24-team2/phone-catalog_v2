fba8f40079cc89a93039f9199eae26bf69a17df2
Iryna Yehorova <yehorova.dev@gmail.com>
2024-09-30 14:45:58 +0300

    debugging

--- src/modules/shared/helpers/getBurgerIcon.ts
+++ src/modules/shared/helpers/getBurgerIcon.ts
@@ -2,12 +2,12 @@ import { Theme } from '@/types/Theme';
 
 const burgerIcons = {
   original: {
-    open: '/images/original/icons/original_burger_open.svg',
-    close: '/images/original/icons/original_burger_close.svg',
+    open: 'images/original/icons/original_burger_open.svg',
+    close: 'images/original/icons/original_burger_close.svg',
   },
   dark: {
-    open: '/images/dark/icons/dark_burger_open.svg',
-    close: '/images/dark/icons/dark_burger_close.svg',
+    open: 'images/dark/icons/dark_burger_open.svg',
+    close: 'images/dark/icons/dark_burger_close.svg',
   },
 };
 
@@ -15,5 +15,8 @@ export const getBurgerIcon = (isMenuOpen: boolean, theme: Theme) => {
   if (!theme) {
     return burgerIcons.original.open;
   }
+  console.log('burgerIcons[theme].open', burgerIcons[theme].open);
+  console.log('theme', theme);
+  console.log('burgerIcons[theme]', burgerIcons[theme]);
   return isMenuOpen ? burgerIcons[theme].close : burgerIcons[theme].open;
 };
